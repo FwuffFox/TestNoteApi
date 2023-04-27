ed6767b6b5c1b30856f81fdc712743ebae3e6a69
FwuffFox <FwuffFox@gmail.com>
2023-04-27 03:14:19 +0300

    Initial commit

---
+++ src/test/java/com/fwufffox/testnoteapi/TestNoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwufffox.testnoteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
